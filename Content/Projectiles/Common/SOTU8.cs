ac0ad153fbc5ba2e9288f92f688c265c3536d2ce
msb165 <75228465+msb165@users.noreply.github.com>
2025-09-19 18:48:22 -0300

    feat: Re-create SOTU8

--- Content/Projectiles/Common/SOTU8.cs
+++ Content/Projectiles/Common/SOTU8.cs
@@ -1,6 +1,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria;
+using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -10,37 +11,76 @@ namespace UniverseOfSwordsMod.Content.Projectiles.Common
     {
         public override void SetStaticDefaults()
         {
-            // DisplayName.SetDefault("Sotu Projectile 8");     //The English name of the projectile
-            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 5;    //The length of old position to be recorded
-            ProjectileID.Sets.TrailingMode[Projectile.type] = 0;        //The recording mode
+            ProjectileID.Sets.TrailCacheLength[Type] = 15;    
+            ProjectileID.Sets.TrailingMode[Type] = 2;      
         }
 
         public override void SetDefaults()
         {
-            Projectile.width = 30;               //The width of projectile hitbox
-            Projectile.height = 75;             //The height of projectile hitbox
-            Projectile.scale = 2.2F;
-            Projectile.aiStyle = 1;             //The ai style of the projectile, please reference the source code of Terraria
-            Projectile.friendly = true;         //Can the projectile deal damage to enemies?
-            Projectile.penetrate = -1;           //How many monsters the projectile can penetrate. (OnTileCollide below also decrements penetrate for bounces as well)
-            Projectile.alpha = 255;             //The transparency of the projectile, 255 for completely transparent. (aiStyle 1 quickly fades the projectile in)
-            Projectile.light = 1f;
-            Projectile.ignoreWater = true;          //Does the projectile's speed be influenced by water?
-            Projectile.tileCollide = false;          //Can the projectile collide with tiles?
-            Projectile.extraUpdates = 1;            //Set to above 0 if you want the projectile to update multiple time in a frame
-            AIType = ProjectileID.Bullet;           //Act exactly like default Bullet
+            Projectile.width = 10;           
+            Projectile.height = 10;          
+            Projectile.scale = 1f;
+            Projectile.aiStyle = -1;          
+            Projectile.friendly = true;       
+            Projectile.penetrate = 1;        
+            Projectile.light = 0.3f;
+            Projectile.ignoreWater = true;      
             Projectile.timeLeft = 300;
+            Projectile.tileCollide = false;
         }
 
-        public virtual void CreateDust()
+        public override void AI()
         {
-            if (Main.rand.Next(4) == 0)
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2 - MathHelper.PiOver4;
+            for (int i = 0; i < 3; i++) 
             {
-                int dust = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.PinkTorch, 0f, 0f, 100, default, 2f);
-                Main.dust[dust].noGravity = true;
-                Main.dust[dust].velocity *= 0.2f;
-                Main.dust[dust].velocity += Projectile.velocity;
+                Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.CrystalPulse, 0f, 0f, 100, default, 1f);
+                dust.noGravity = true;
+                dust.velocity *= 0.6f;
             }
+        }
+
+        public override void OnKill(int timeLeft)
+        {
+            Projectile.Damage();
+            for (int i = 0; i < 5; i++)
+            {
+                Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.PinkCrystalShard);
+                dust.alpha = 100;
+                dust.noGravity = true;
+                Dust dust2 = dust;
+                dust2.velocity *= 1.5f;
+                dust2 = dust;
+                dust2.scale *= 0.9f;
+            }
+            if (Projectile.owner == Main.myPlayer)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    float speedX = -Projectile.velocity.X * Main.rand.Next(40, 70) * 0.01f + Main.rand.Next(-20, 21) * 0.4f;
+                    float speedY = -Projectile.velocity.Y * Main.rand.Next(40, 70) * 0.01f + Main.rand.Next(-20, 21) * 0.4f;
+                    Projectile.NewProjectile(Projectile.GetSource_Death(), Projectile.position.X + speedX, Projectile.position.Y + speedY, speedX, speedY, ModContent.ProjectileType<PinkCrystalShard>(), Projectile.damage, 0f, Projectile.owner);
+                }
+            }
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D texture = TextureAssets.Projectile[Type].Value;
+            Vector2 origin = new(texture.Width - 4f, texture.Height / 4 - 4f);
+            Color drawColor = Color.White with { A = 200 } * Projectile.Opacity;
+            Color trailColor = drawColor;
+            SpriteEffects spriteEffects = Projectile.spriteDirection == 1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
+
+            for (int i = 0; i < Projectile.oldPos.Length; i++)
+            {
+                trailColor *= 0.7f;
+                Main.spriteBatch.Draw(texture, Projectile.oldPos[i] + Projectile.Size / 2 - Main.screenPosition, null, trailColor, Projectile.oldRot[i], origin, Projectile.scale, spriteEffects, 0f);
+            }
+
+            Main.spriteBatch.Draw(texture, Projectile.Center - Main.screenPosition, null, drawColor, Projectile.rotation, origin, Projectile.scale, spriteEffects, 0f);
+
+            return false;
         }
     }
 }
