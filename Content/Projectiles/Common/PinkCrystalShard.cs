1dc600603c5fc0bc809f21a54c066d4b2d42fda9
msb165 <75228465+msb165@users.noreply.github.com>
2025-09-19 18:49:16 -0300

    feat: Add "Pink Crystal Shard"

---
+++ Content/Projectiles/Common/PinkCrystalShard.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace UniverseOfSwordsMod.Content.Projectiles.Common
+{
+    public class PinkCrystalShard : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.Size = new(6);
+            Projectile.aiStyle = ProjAIStyleID.CrystalShard;
+            Projectile.friendly = true;
+            Projectile.penetrate = 1;
+            Projectile.light = 0.5f;
+            Projectile.alpha = 50;
+            Projectile.scale = 1.2f;
+            Projectile.timeLeft = 600;
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.tileCollide = false;
+        }
+
+        public override Color? GetAlpha(Color lightColor) => Color.White with { A = 0 } * Projectile.Opacity;
+    }
+}
