5b6f4527bc31705195768788bacaef85a66001c2
msb165 <75228465+msb165@users.noreply.github.com>
2025-10-20 20:48:38 -0300

    Update TrueTerrablade.cs

--- Content/Projectiles/Common/TrueTerrablade.cs
+++ Content/Projectiles/Common/TrueTerrablade.cs
@@ -1,18 +1,23 @@
-using System;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using UniverseOfSwords.Utilities;
 
 namespace UniverseOfSwords.Content.Projectiles.Common
 {
     public class TrueTerrablade : ModProjectile
     {
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailCacheLength[Type] = 30;
+            ProjectileID.Sets.TrailingMode[Type] = 2;
+        }
+
         public override void SetDefaults()
         {
-            Projectile.width = 50;
-            Projectile.height = 30;
+            Projectile.width = 32;
+            Projectile.height = 32;
             Projectile.scale = 1f;
             Projectile.friendly = true;
             Projectile.penetrate = 1;
@@ -20,38 +25,42 @@ namespace UniverseOfSwords.Content.Projectiles.Common
             Projectile.DamageType = DamageClass.Melee;
             Projectile.tileCollide = true;
             Projectile.ignoreWater = true;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 20;
         }
 
         public override void AI()
         {
-            Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Terra);
-            dust.noGravity = true;
-            dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Terra);
-            dust.noGravity = true;
-            dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Terra);
-            dust.scale = 1f;
-            Projectile.rotation = Projectile.velocity.ToRotation() + (float)(Math.PI / 4);
+            for (int i = 0; i < 3; i++)
+            {
+                Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Terra);
+                dust.scale = 0.5f;
+            }
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver4;
         }
 
-        public override bool PreDraw(ref Color lightColor) //this is where the animation happens
+        public override bool PreDraw(ref Color lightColor)
         {
-            Projectile.frameCounter++; //increase the frameCounter by one
-            if (Projectile.frameCounter >= 6) //once the frameCounter has reached 10 - change the 10 to change how fast the projectile animates
-            {
-                Projectile.frame++; //go to the next frame
-                Projectile.frameCounter = 0; //reset the counter
-                if (Projectile.frame > 5) //if past the last frame
-                    Projectile.frame = 0; //go back to the first frame
-            }
-            return true;
+            UniverseUtils.Drawing.DrawWithAfterImages(Projectile, Color.White with { A = 0 } * Projectile.Opacity);
+            return false;
         }
 
         public override void OnKill(int timeLeft)
         {
-            for (int i = 0; i < 19; i++)
+            Projectile.Damage();
+            for (int i = 0; i < 15; i++)
             {
-                Vector2 spawnVel = (Vector2.UnitY * 8f).RotatedBy(i * MathHelper.TwoPi / 19f) * new Vector2(2f, 1f);
-                Projectile.NewProjectile(Projectile.GetSource_Death(), Projectile.position, spawnVel.RotatedBy(Projectile.velocity.ToRotation()), ProjectileID.TerraBeam, Projectile.damage, 0, Projectile.owner, 0f, 0f);
+                Dust dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.Terra, Scale: 2f);
+                dust.noGravity = true;
+            }
+
+            if (Main.rand.NextBool(6))
+            {
+                for (int i = 0; i < 16; i++)
+                {
+                    Vector2 spawnVel = (Vector2.UnitY * 8f).RotatedBy(i * MathHelper.TwoPi / 16f) * new Vector2(2f, 1f);
+                    Projectile.NewProjectile(Projectile.GetSource_Death(), Projectile.position, spawnVel.RotatedBy(Projectile.velocity.ToRotation()), ProjectileID.TerraBeam, Projectile.damage / 2, 0, Projectile.owner, 0f, 0f);
+                }
             }
         }
     }
