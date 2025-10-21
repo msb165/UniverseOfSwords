6a0575ef64f11cfe03bfa19eab2fb3bc2b380162
msb165 <75228465+msb165@users.noreply.github.com>
2025-10-20 21:06:24 -0300

    Update ChangeArmRotation.cs

--- Common/GlobalItems/ChangeArmRotation.cs
+++ Common/GlobalItems/ChangeArmRotation.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using System.Linq;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -9,7 +10,8 @@ namespace UniverseOfSwords.Common.GlobalItems
     {
         public override void UseStyle(Item item, Player player, Rectangle heldItemFrame)
         {
-            if ((item.DamageType == DamageClass.Melee || item.DamageType == DamageClass.MeleeNoSpeed) && !item.noMelee && !item.noUseGraphic && item.useStyle == ItemUseStyleID.Swing)
+            int[] allowedTypes = [ItemID.NightsEdge, ItemID.TerraBlade, ItemID.TrueNightsEdge, ItemID.Excalibur, ItemID.TrueExcalibur];
+            if ((item.DamageType == DamageClass.Melee || item.DamageType == DamageClass.MeleeNoSpeed) && !item.noMelee && !item.noUseGraphic && item.useStyle == ItemUseStyleID.Swing || allowedTypes.Contains(item.type))
             {
                 float rotation = player.itemRotation - MathHelper.PiOver2 - MathHelper.PiOver4;
                 if (player.direction == -1)
