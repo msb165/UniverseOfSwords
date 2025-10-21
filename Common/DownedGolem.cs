91f0b45912e6ab554ae5f044f9db945df5bdab8e
msb165 <75228465+msb165@users.noreply.github.com>
2025-10-20 21:05:49 -0300

    Create DownedGolem.cs

---
+++ Common/DownedGolem.cs
@@ -0,0 +1,12 @@
+﻿using Terraria;
+using Terraria.GameContent.ItemDropRules;
+
+namespace UniverseOfSwords.Common
+{
+    public class DownedGolem : IItemDropRuleCondition, IProvideItemConditionDescription
+    {
+        public bool CanDrop(DropAttemptInfo info) => NPC.downedGolemBoss;
+        public bool CanShowItemDropInUI() => true;
+        public string GetConditionDescription() => null;
+    }
+}
