7a4e57c6ce0a2bec2ee047ad6dc3b8d8191596f4
msb165 <75228465+msb165@users.noreply.github.com>
2025-10-20 21:05:46 -0300

    Create UniverseGlobalProjectile.cs

---
+++ Common/UniverseGlobalProjectile.cs
@@ -0,0 +1,10 @@
+﻿using Terraria.ModLoader;
+
+namespace UniverseOfSwords.Common
+{
+    public class UniverseGlobalProjectile : GlobalProjectile
+    {
+        public override bool InstancePerEntity => true;
+        public bool decreasedDamage = false;
+    }
+}
