60e4ff3182d9313fac6010305c66bab4c97bfea7
msb165 <75228465+msb165@users.noreply.github.com>
2025-10-26 19:25:42 -0300

    Update UniverseOfSwords.cs

--- UniverseOfSwords.cs
+++ UniverseOfSwords.cs
@@ -4,8 +4,22 @@ namespace UniverseOfSwords
 {
     public class UniverseOfSwords : Mod
     {
+        public static UniverseOfSwords Instance;
+        public Mod CalamityMod = null;
+        public Mod ThoriumMod = null;
         public UniverseOfSwords()
         {
+            
+        }
+
+        public override void Load()
+        {
+            Instance = this;
+            ThoriumMod = null;
+            ModLoader.TryGetMod("ThoriumMod", out ThoriumMod);
+            CalamityMod = null;
+            ModLoader.TryGetMod("CalamityMod", out CalamityMod);
+            base.Load();
         }
     }
 }
